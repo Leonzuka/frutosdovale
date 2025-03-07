b6ce24f915b14c230d145a9ff2975879f2dda364
Leonardo Feitosa <81185545+Leonzuka@users.noreply.github.com>
2025-03-07 19:17:51 -0300

    Update app.py

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = database_url or 'mysql://root:CDgpcTOfpv
 
 # Ajuste para funcionamento em produção
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 8080))
+    port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
 
 #Start URL
